709d1453ef1c26d53b2a27c25624431e8dcea2ee
LordCodex <michaelolaitan6@gmail.com>
2023-11-24 20:11:17 +0100

    last update unable to do editing form

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,11 +2,6 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+  const dataForm = ref([]);
+  return { dataForm }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import {computed, ref, onMounted } from 'vue'
+import { storeToRefs } from "pinia";
 import { showNotification } from "@/utils/notify"
-
-
+import { useCounterStore } from "@/stores/counter";
+const store = useCounterStore();
+const { dataForm } = storeToRefs(store)
 // export  defineComponent({
 //   components: {
 //     DarkMode,
@@ -18,17 +20,57 @@ const expenseAmount = ref("");
 const expenseDescription = ref('');
 const expenseTitle = ref('');
 const expenseDate = ref('');
-const expenseList = ref([]);
-
+const  expenseList = ref([]);
+let editedItem =  computed(() => findData(editIndex.value));
+const selectedItem = ref(null);
+let modalOpen = ref(false)
+let editIndex = ref(null)
+  // Shallow copy of the original array
+  const modifiedArray = ref([]);
 
 // Methods
+const openModal = (indexes) => {
+  modalOpen.value = ref(true);
+  editIndex.value = indexes;
+}
+
+const closeModal = () => {
+  modalOpen.value = !modalOpen;
+}
+const findData = (indexes) => {
+  modifiedArray.value = [...expenseList.value]; // Shallow copy
+  selectedItem.value = modifiedArray.value.find(item => item.id === indexes);
+
+  return selectedItem.value;
+}
+// const updateExpense = () => {
+//   if (editedItem.amount < expenseAmount) {
+//     if (editIndex !== null) {
+//       expenseList.value[editIndex] = editedItem;
+//       showNotification({
+//         message: "Budget Sucessfully Added",
+//         success: true,
+//       });
+//       closeModal();
+//     }
+//     else {
+//       showNotification({
+//         message: 'Insufficient Balance..',
+//         success: false,
+//       });
+//     }
+
+//   }
+// }
+
+
 const addBudget = () => {
   let amount = parseFloat(budgetAmount.value);
   if (isNaN(amount) || amount <= 0) {
     showNotification({
-          message: 'Please enter a valid expense amount.',
-          success: false,
-        });
+      message: 'Please enter a valid expense amount.',
+      success: false,
+    });
     return;
   }
 
@@ -36,9 +78,9 @@ const addBudget = () => {
   remainingBudget.value += amount;
   budget.value += amount;
   showNotification({
-          message: "Budget Sucessfully Added",
-          success: true,
-        });
+    message: "Budget Sucessfully Added",
+    success: true,
+  });
   budgetAmount.value = "";
 
 };
@@ -47,17 +89,17 @@ const addExpense = () => {
   const amount = parseFloat(expenseAmount.value);
   if (isNaN(amount) || amount <= 0) {
     showNotification({
-          message: 'Please enter a valid expense amount.',
-          success: false,
-        });
+      message: 'Please enter a valid expense amount.',
+      success: false,
+    });
     return;
   }
 
   if (amount > remainingBudget.value) {
     showNotification({
-          message: 'Insufficient Balance..',
-          success: false,
-        });
+      message: 'Insufficient Balance..',
+      success: false,
+    });
     return;
   }
 
@@ -65,15 +107,16 @@ const addExpense = () => {
   remainingBudget.value -= amount;
 
   expenseList.value.push({
+    id:getRandomInt(1000),
     amount,
     description: expenseDescription.value,
     title: expenseTitle.value,
     date: expenseDate.value,
   });
   showNotification({
-          message: "Expense Sucessfully Added",
-          success: true,
-        });
+    message: "Expense Sucessfully Added",
+    success: true,
+  });
   // Clear input fields
   expenseAmount.value = "";
   expenseDescription.value = '';
@@ -86,20 +129,20 @@ const removeExpense = (index) => {
 
   expenseList.value.splice(index, 1);
   showNotification({
-          message: "Expense Sucessfully Removed",
-          success: true,
-        });
+    message: "Expense Sucessfully Removed",
+    success: true,
+  });
 };
 
 const reset = () => {
-   budget.value = 0;
-   remainingBudget.value = 0;
-   expenses.value = 0;
-   expenseList.value.splice(0)
-   showNotification({
-          message: "All data Cleared",
-          success: true,
-        });
+  budget.value = 0;
+  remainingBudget.value = 0;
+  expenses.value = 0;
+  expenseList.value.splice(0)
+  showNotification({
+    message: "All data Cleared",
+    success: true,
+  });
 };
 const setGreeting = () => {
   const currentTime = new Date().getHours();
@@ -112,36 +155,42 @@ const setGreeting = () => {
     greeting.value = 'Good Evening!';
   }
 };
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+}
 
-const  getMinDate = () => {
-      const today = new Date();
-      const year = today.getFullYear();
-      let month = today.getMonth() + 1; // months are zero-indexed
-      let day = today.getDate();
+const getMinDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  let month = today.getMonth() + 1; // months are zero-indexed
+  let day = today.getDate();
 
-      // Add leading zero if needed
-      month = month < 10 ? `0${month}` : month;
-      day = day < 10 ? `0${day}` : day;
+  // Add leading zero if needed
+  month = month < 10 ? `0${month}` : month;
+  day = day < 10 ? `0${day}` : day;
 
-      return `${year}-${month}-${day}`;
-    }
-    const minDate= getMinDate();
+  return `${year}-${month}-${day}`;
+}
+const minDate = getMinDate();
 onMounted(() => {
   setGreeting();
   // expenseList.value.push({
-  //   amount:0,
+    
+  //   amount: 0,
   //   description: "KODEXCODES",
   //   title: "New Expenses",
   //   date: "12-5-2020",
   // });
 });
+
+
 </script>
 
 <template>
   <main>
     <nav>
       <div class="nav-title">
-        Expense Tracker 
+        Expense Tracker
       </div>
 
     </nav>
@@ -189,7 +238,49 @@ onMounted(() => {
           </div>
         </div>
 
-        
+        <!-- The Modal -->
+        <div v-if="modalOpen">
+          <div id="myModal" class="modal">
+
+
+            <div class="addExpense modal-content flex fl-col">
+
+              <div style="display:flex; justify-content:space-between;width:100%">
+                <div class="expense-title">Edit Expense</div>
+                <span class="close" @click="closeModal">&times;</span>
+              </div>
+              <form @submit.prevent="updateExpense" class="flex fl-col expenseForm">
+                <div>
+                  
+                  <label>Expense Title: </label>
+                  <input type="text" class="input-field" id="expTitle" v-model="editedItem.title"
+                    placeholder="Enter your expense title.." required maxlength="15" autocomplete="off">
+                </div>
+                <div>
+                  <label>Expense Description: </label>
+                  <input type="text" class="input-field" id="expDesc" v-model="editedItem.description"
+                    placeholder="Enter your expense Desc.." required maxlength="30" autocomplete="off">
+                </div>
+                <div>
+                  <label>Expense Amount: </label>
+                  <input type="number" class="input-field" v-model="editedItem.amount" id="expAmount"
+                    placeholder="Enter your expense.." required autocomplete="off">
+                </div>
+
+                <div>
+                  <label>Current Date: </label>
+                  <input type="date" class="input-field" v-model="editedItem.date" id="date" :min="minDate"
+                    placeholder="Enter your expense.." required autocomplete="off">
+                </div>
+
+                <button class="primary-btn flex" id="addExpense" type="submit" >Update Expenses<ion-icon
+                    name="arrow-forward-outline"></ion-icon></button>
+              </form>
+            </div>
+
+
+          </div>
+        </div>
         <div class="expenseSection flex">
 
 
@@ -226,7 +317,7 @@ onMounted(() => {
 
                 <!-- Expense Items -->
 
-                <li v-for="(expense, index) in expenseList" class="item flex">
+                <li v-for="(expense, index) in expenseList" :key="expense.id" class="item flex">
                   <div class="list-expense-title-desc text-elipses flex">
                     <div class="flex fl-col list-text">
                       <div class="list-expense-title">{{ expense.title }}</div>
@@ -237,7 +328,8 @@ onMounted(() => {
                       <div class="date">{{ expense.date }}</div>
                     </div>
                   </div>
-                  <button id="delete" @click="removeExpense(index)">X</button>
+                  <button id="edit" @click="openModal(expense.id)"><ion-icon name="create-outline"></ion-icon></button>
+                  <button id="delete" @click="removeExpense(expense.id)">X</button>
                 </li>
 
 
@@ -267,8 +359,8 @@ onMounted(() => {
 
               <div>
                 <label>Current Date: </label>
-                <input type="date" class="input-field" v-model="expenseDate" id="date"  :min="minDate"  placeholder="Enter your expense.."
-                  required autocomplete="off">
+                <input type="date" class="input-field" v-model="expenseDate" id="date" :min="minDate"
+                  placeholder="Enter your expense.." required autocomplete="off">
               </div>
 
               <button class="primary-btn flex" id="addExpense" type="submit">Add Expense <ion-icon
